2aa0daa534a002d2c058479f7088d510e800c2f1
Foysol Ahmed <foysol.sahi@gmail.com>
2020-12-25 18:26:35 +0600

    basic setup

---
+++ src/providers/data/data.ts
@@ -0,0 +1,13 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataProvider {
+
+  constructor(public http: HttpClient) {
+    
+  }
+
+  
+
+}
